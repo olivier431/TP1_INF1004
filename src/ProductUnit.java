b662dbb4cedb2639a9bee34f61d4a22e6ab6ee55
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2024-02-14 12:27:53 -0500

    Feat: Added ProductUnit

---
+++ src/ProductUnit.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class ProductUnit {
+
+    private Date expirationDate;
+
+    public ProductUnit(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
